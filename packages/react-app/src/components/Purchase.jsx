d6c39e7f360aee23e7053dedea3fb4fe6fd28463
Isaac Patka <ipatka@gmail.com>
2021-03-20 12:48:05 -0400

    purchase interactions working

--- packages/react-app/src/components/Purchase.jsx
+++ packages/react-app/src/components/Purchase.jsx
@@ -3,9 +3,9 @@ import { formatEther, parseEther } from "@ethersproject/units";
 import smallLogo from "../assets/logo-small.svg";
 import "./Purchase.css";
 
-export default function Purchase({ availTokens, wallet, currPrice }) {
+export default function Purchase({ availTokens, yourBalance, tokenAddress, currPrice, tx, writeContracts }) {
   const purchaseToken = () => {
-    // TODO: buyToken();
+    tx( writeContracts.TokenSale.buyToken(tokenAddress, 1, {value: currPrice}) )
   };
 
   return (
@@ -13,8 +13,9 @@ export default function Purchase({ availTokens, wallet, currPrice }) {
       <img alt="" src={smallLogo} />
       <h1>Only {availTokens.toNumber()} Remaining</h1>
       <p>Current Price: {formatEther(currPrice)}ETH</p>
-      {wallet ? (
-        <button onClick={formatEther(purchaseToken)}>Purchase</button>
+      <p>Your Balance {yourBalance.toNumber()}</p>
+      {writeContracts ? (
+        <button onClick={purchaseToken}>Purchase</button>
       ) : (
         <p className="no-wallet">Connect your wallet to puchase</p>
       )}
