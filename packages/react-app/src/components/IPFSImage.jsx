f37d7dc7d634f2e7ba7bd32b12d8f12b403c0c65
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-03-20 10:43:03 -0700

    Switch default image to svg

--- packages/react-app/src/components/IPFSImage.jsx
+++ packages/react-app/src/components/IPFSImage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import defaultUri from "../assets/default.png";
+import defaultUri from "../assets/default.svg";
 
 export default function IPFSImage({ hash }) {
   const [uri, setUri] = useState(defaultUri);
