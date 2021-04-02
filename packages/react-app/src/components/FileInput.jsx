42b57d5a6be8321c1ee6ebc72dbbbde2ebcc42c2
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-04-02 12:48:00 -0700

    Image Upload and cards

--- packages/react-app/src/components/FileInput.jsx
+++ packages/react-app/src/components/FileInput.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as UploadIcon } from "assets/upload-icon.svg";
 import "./FileInput.scss";
 
-export default function fileInput({ label, name }) {
+export default function FileInput({ label, name }) {
+  const [file, setFile] = useState();
+
+  const handleUpload = event => {
+    try {
+      const img = URL.createObjectURL(event.nativeEvent.target.files[0]);
+      setFile(img);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="file-input-container">
       <h4>{label}</h4>
-      <label>
+      <label className={file ? "uploaded-image" : null}>
+        <div>
+          <img alt="" src={file} />
+        </div>
         <UploadIcon />
         <p>PNG, GIF, WEBP or MP4</p>
-        <input name={name} type="file" accept=".png|.gif|.webp|.mp4" />
+        <input onChange={handleUpload} name={name} type="file" accept=".png,.gif,.webp,.mp4" />
       </label>
     </div>
   );
--- packages/react-app/src/pages/Mint.jsx
+++ packages/react-app/src/pages/Mint.jsx
@@ -2,26 +2,45 @@ import React, { useState } from "react";
 import { FileInput, TextInput } from "components";
 import { ReactComponent as EditIcon } from "assets/edit-icon.svg";
 import { ReactComponent as AddIcon } from "assets/add-icon.svg";
+import { ReactComponent as TrashIcon } from "assets/trash-icon.svg";
+import noImage from "assets/no-image.svg";
 import "./Mint.scss";
 
 const MAX_ARTWORKS = 6;
 
 export default function Mint() {
   const [artworks, setArtworks] = useState([]);
-  const [editting, setEditting] = useState(-1);
+  const [editing, setEditing] = useState(-1);
 
-  const renderCard = ({ title, image, description }) => (
-    <div className="art-card">
-      <img alt="preview" src={image} />
-      <div>
-        <EditIcon />
-      </div>
-      <div>
-        <h4>{title || "Title"}</h4>
-        <p>{description || "Description"}</p>
+  const renderCard = ({ title, image, description }, index) => (
+    <div className="card">
+      <div className="art-card">
+        <img alt="preview" src={image || noImage} />
+        <div
+          className={editing === index ? "action-icon editing" : "action-icon"}
+          onClick={() => {
+            if (editing === index) {
+              setArtworks(artworks.filter((item, ind) => ind !== index));
+              setEditing(-1);
+            } else {
+              setEditing(index);
+            }
+          }}
+        >
+          {editing === index ? <TrashIcon /> : <EditIcon />}
+        </div>
+        <div className="info-container">
+          <h4>{title || "Title"}</h4>
+          <p>{description || "Description"}</p>
+        </div>
       </div>
     </div>
   );
+
+  const addCard = () => {
+    setArtworks([...artworks, {}]);
+    setEditing(artworks.length);
+  };
 
   return (
     <div className="create-collection">
@@ -36,15 +55,19 @@ export default function Mint() {
       />
       <div className="artworks">
         <h4>Artworks</h4>
-        {artworks?.map(renderCard)}
-        {artworks.length < MAX_ARTWORKS ? (
-          <div className="new-card">
-            <AddIcon />
-            <p>Add New</p>
-          </div>
-        ) : null}
+        <div className="artwork-card-container">
+          {artworks?.map(renderCard)}
+          {artworks.length < MAX_ARTWORKS ? (
+            <div className="card">
+              <div className="new-card" onClick={addCard}>
+                <AddIcon />
+                <p>Add New</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
       </div>
-      {editting ? <div /> : null}
+      {editing ? <div /> : null}
       <button>Mint Collection</button>
     </div>
   );
