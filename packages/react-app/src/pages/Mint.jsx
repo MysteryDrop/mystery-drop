e6d7aaa3936757a0e084945be7d7fca137335c20
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-04-06 11:12:34 -0700

    Fix modal styling

--- packages/react-app/src/pages/Mint.jsx
+++ packages/react-app/src/pages/Mint.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FileInput, TextInput, DateTimeInput } from "components";
+import { ReactComponent as ModalIndicator } from "assets/modal-indicator.svg";
 import { ReactComponent as EditIcon } from "assets/edit-icon.svg";
 import { ReactComponent as AddIcon } from "assets/add-icon.svg";
 import { ReactComponent as TrashIcon } from "assets/trash-icon.svg";
@@ -16,6 +17,7 @@ export default function Mint() {
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
   const [dropDate, setDropDate] = useState();
+  const modal = useRef();
 
   const submit = () => {
     console.log(
@@ -35,8 +37,8 @@ export default function Mint() {
   };
 
   const renderCard = ({ title, image, description }, index) => (
-    <div className="card">
-      <div className="art-card">
+    <div className="card" key={index}>
+      <div className="art-card fade-in">
         <img alt="preview" src={image || noImage} />
         <div
           className={editing === index ? "action-icon editing" : "action-icon"}
@@ -99,6 +101,56 @@ export default function Mint() {
     ]);
   };
 
+  const setModalPosition = () => {
+    const indicator = document.querySelector(".modal-indicator");
+
+    const modal = document.querySelector(".modal-wrapper");
+    const editingCardRect = document.querySelectorAll(".art-card")[editing].getBoundingClientRect();
+    const artworksRect = document.querySelector(".artworks").getBoundingClientRect();
+    const heightOffset = editingCardRect.bottom - artworksRect.top;
+    modal.style.top = `${heightOffset + 10}px`;
+
+    if (window.innerWidth < 500) {
+      switch (editing % 2) {
+        case 0:
+          indicator.style.marginLeft = "-25%";
+          break;
+        case 1:
+          indicator.style.marginLeft = "25%";
+          break;
+      }
+    } else {
+      switch (editing % 3) {
+        case 0:
+          indicator.style.marginLeft = `-${100 / 3}%`;
+          break;
+        case 1:
+          indicator.style.marginLeft = 0;
+          break;
+        case 2:
+          indicator.style.marginLeft = `${100 / 3}%`;
+          break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (modal.current) {
+      if (editing < 0) {
+        modal.current.classList.add("collapsed");
+      } else {
+        setModalPosition();
+        modal.current.classList.remove("collapsed");
+      }
+    }
+  }, [editing]);
+
+  useEffect(() => {
+    document.querySelectorAll(".fade-in").forEach(element => {
+      element.classList.add("visible");
+    });
+  }, [artworks]);
+
   return (
     <div className="create-collection">
       <h1>Create Collection</h1>
@@ -126,28 +178,29 @@ export default function Mint() {
           {artworks?.map(renderCard)}
           {artworks.length < MAX_ARTWORKS ? (
             <div className="card">
-              <div className="new-card" onClick={addCard}>
+              <div className="new-card fade-in" onClick={addCard}>
                 <AddIcon />
                 <p>Add New</p>
               </div>
             </div>
           ) : null}
         </div>
-        {editing >= 0 ? (
-          <div className="edit-modal" key={editing}>
+        <div className="modal-wrapper collapsed" key={editing} ref={modal}>
+          <ModalIndicator className="modal-indicator" />
+          <div className="edit-modal">
             <ExitIcon className="exit" onClick={() => setEditing(-1)} />
             <FileInput
               label="Preview Image"
               name={`artwork-${editing}`}
               onChange={setArtworkImage}
-              defaultImg={artworks[editing].image}
+              defaultImg={artworks[editing]?.image}
             />
             <TextInput
               label="Name"
               placeholder="Eg. Splash"
               name={`title-${editing}`}
               onChange={setArtworkName}
-              defaultText={artworks[editing].title}
+              defaultText={artworks[editing]?.title}
             />
             <TextInput
               label="Description"
@@ -155,10 +208,10 @@ export default function Mint() {
               multiline={true}
               name={`description-${editing}`}
               onChange={setArtworkDesc}
-              defaultText={artworks[editing].description}
+              defaultText={artworks[editing]?.description}
             />
           </div>
-        ) : null}
+        </div>
       </div>
       <DateTimeInput
         label="Drop Date"
