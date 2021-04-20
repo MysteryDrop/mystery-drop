f8d3108a446d3b925fcc786274dd712ad46387f5
Owen Murovec <owenmurovec@Owens-MacBook-Pro.local>
2021-04-20 11:12:13 -0700

    Restrict multiple auth requests

--- packages/react-app/src/components/AuthRequired.jsx
+++ packages/react-app/src/components/AuthRequired.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { Web3Provider } from "@ethersproject/providers";
 
 import { login } from "../util/auth";
@@ -6,16 +6,20 @@ import { ReactComponent as UserImg } from "assets/person-fill.svg";
 import "./AuthRequired.scss";
 
 export default function AuthRequired({ provider, jwtAuthToken, setJwtAuthToken }) {
+  const [awaitingJwt, setAwaitingJwt] = useState(false);
+
   const connect = useCallback(async () => {
+    setAwaitingJwt(true);
     const token = await login({ provider });
     setJwtAuthToken(token);
+    setAwaitingJwt(false);
   }, [setJwtAuthToken]);
 
   return (
     <div className="auth-required">
       <UserImg />
       <p>Account Required</p>
-      <a className="button-alt is-primary" onClick={connect}>
+      <a className="button-alt is-primary" onClick={awaitingJwt ? null : connect}>
         Sign In
       </a>
     </div>
